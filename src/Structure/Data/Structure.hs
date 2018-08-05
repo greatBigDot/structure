875f54460fe93a35ec882947de9acbc22f77700d
greatBigDot <greatBigDot@gmail.com>
2018-08-04 22:19:46 -0400

    A representation of various algebraic structures as typeclasses. Laws unverified.

---
+++ src/Structure/Data/Structure.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_HADDOCK show-extensions #-}
+-- |
+-- Module      : Structure.Data.Structure
+-- Description : The primary datatype of structure: a representation of various algebraic structures as typeclasses. Laws unverified.
+-- Copyright   : (c) greatBigDot, 2018
+-- License     : BSD-3-Clause
+-- Maintainer  : greatBigDot@gmail.com
+-- Stability   : experimental
+--
+module Structure.Data.Structure (
+                                ) where
+
